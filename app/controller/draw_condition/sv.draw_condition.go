c819d9f0bb3d51eb2a178a950ea18835936b051b
shootle48 <86481062+shootle48@users.noreply.github.com>
2025-04-25 00:15:07 +0700

    fix drawCondition

--- app/controller/draw_condition/sv.draw_condition.go
+++ app/controller/draw_condition/sv.draw_condition.go
@@ -9,6 +9,8 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+
+	"github.com/uptrace/bun"
 )
 
 func (s *Service) Create(ctx context.Context, req request.CreateDrawCondition) (*model.DrawCondition, bool, error) {
@@ -144,7 +146,7 @@ func (s *Service) PreviewPlayer(ctx context.Context, req request.PreviewPlayers)
 		Where("p.deleted_at IS NULL")
 
 	if len(req.FilterPosition) > 0 {
-		query = query.Where("p.position = ?", req.FilterPosition)
+		query = query.Where("p.position IN (?)", bun.In(req.FilterPosition))
 	}
 
 	if req.FilterStatus == "received" {
